d8d62c459b1877fcb2f3a511431fab3fa9abbb37
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2015-12-30 13:11:01 -0500

    Update index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Xenoblade Augment Info</title>
+		<link rel="stylesheet" type="text/css" href="styles.css" title="Default Styles" media="screen"/>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans" title="Font Styles"/>
+		<?php include "general_functions.php"; ?>
+	</head>
+	
+	<body link="#E2E2E2" vlink="#ADABAB">
+		<center><div class="container">
+	
+		
+			<header>
+		
+				<?php print_header(); ?>
+				
+			</header>
+			
+			<article style="color:#FFFFFF;">
+				<p>
+					<!-- <center><img src="logo_big.png"></center> Insert Main Logo here -->
+					
+					<hr/>
+					<center><h1>Xenoblade Chronicles X</h1></center>
+					<hr/>
+					<p>
+						
+						<div class="box">
+							<p>
+								<center><h3>Xenoblade Hub</h3></center>
+								
+								Main page for all my Xenoblade stuff. Here's a list of everything I've done (and may do):
+                                
+                                <ul>
+                                    <li><a href ="augment_search.php">Augment search engine</a></li>
+                                    <li>Reverse augment lookup (click on or type in a material and all matching augments will be listed. These can then be clicked on for more details)
+                                    <li>Skell frame list. You would be able to click on the skell frame and view all details about it (just like the augment search)</li>
+                                    <li>Skell superweapon list</li>
+                                    <li>Craftable ground armor list (will probably skip weapons as they all seem useless)</li>
+                                    <li><a href ="overdrive.php">Ground Overdrive Guide</a>, taken from reddit (and edited) for convenience. Source link included in document.</li>
+                                </ul>
+								
+							</p>
+						</div>
+                                
+                            </p>
+                        </div>
+
+                    </p>
+
+                </p>
+			
+			
+			</article>
+			
+			<div class="paddingBottom">
+			</div>
+			
+			<footer>
+				2016 birdonwheels5.
+			</footer>
+		</div>
+	</body>
+	
+</html>
