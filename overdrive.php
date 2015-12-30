0121e14352d06227da9259fde3dd9524e002f681
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2015-12-30 13:11:36 -0500

    Create overdrive.php

---
+++ overdrive.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Xenoblade Augment Info</title>
+		<link rel="stylesheet" type="text/css" href="styles.css" title="Default Styles" media="screen"/>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans" title="Font Styles"/>
+		<?php include "general_functions.php"; ?>
+	</head>
+	
+	<body link="#E2E2E2" vlink="#ADABAB">
+		<center><div class="container">
+	
+		
+			<header>
+		
+				<?php print_header(); ?>
+				
+			</header>
+			
+			<article style="color:#FFFFFF;">
+				<p>
+					<!-- <center><img src="logo_big.png"></center> Insert Main Logo here -->
+					
+					<hr/>
+					<center><h1>Xenoblade Chronicles X</h1></center>
+					<hr/>
+					<p>
+						
+						<div class="box">
+							<p>
+								<center><h3>Overdrive Explained</h3></center>
+								
+								Overdrive is probably the most powerful tool at your disposal, but it's also very poorly explained in game and the manual.
+                                
+                                <h3>Basics</h3>
+                                <ul>
+                                    <li>Overdrive costs 3000 TP to activate and is made available upon completion of chapter 5.</li>
+                                    <li>There are three key features of overdrive: the counter, its color, and the time remaining.</li>
+                                    <li>The counter starts at zero and increases by one for each hit landed up to a max of 100 and affects the potency of many of overdrive's bonuses.</li>
+                                    <li>The color of the counter is always that of the last art used.</li>
+                                    <li>The time remaining is indicated by blocks to the right of the counter.</li>
+                                    <li>Each block appears to be one second. If you have more time than blocks they will remain full until the time dips under 20s.</li>
+                                    <li>Additionally a moment before overdrive expires the overdrive music fades, which can be a useful indicator that you need to extend it ASAP.</li>
+                                    <li>Overdrive can be renewed by selecting the counter. This expends 3000 TP, has a short animation, can be performed anytime even during art animations, and can trigger or complete soul voices.</li>
+                                </ul>
+                                <h3>Constant Bonuses</h3>
+                                <ul>
+                                    <li>Tertiary cool downs enabled</li>
+                                    <li>Secondary and tertiary cool downs fill no matter what weapon is equipped</li>
+                                    <li>Recast speed x (2.5+.05 x counter value)</li>
+                                    <li>Damage x (1 + .05 x counter value), only active when counter is orange, yellow, or purple</li>
+                                    <li>Enemy resistances decreased by counter value, only active when counter is orange, yellow, or purple</li>
+                                </ul>
+                                <h3>Color bonuses</h3>
+                                
+                                These occur when an art of a certain color is used regardless of the color of the counter
+                                <ul>
+                                    <li><b>Green</b>: Overdrive extended by .05 s x counter value</li>
+                                    <li><b>Blue</b>: Overdrive extended by .1 s x counter value</li>
+                                    <li><b>Purple</b>: Gain 10 tp x counter value per hit</li>
+                                </ul>
+                                
+                                <h3>Color combinations</h3>
+                                
+                                These occur when the counter is a certain color and an art of a specific color is used. The effect is applied to the art used.
+                                <ul>
+                                    <li>Yellow to orange and orange to yellow: Overdrive counter does not increase</li>
+                                    <li>Green to orange, yellow, or purple: Overdrive counter increases by 2x the number of hits</li>
+                                    <li>Purple to orange or yellow: extra appendage damage</li>
+                                    <li>Blue to Green: HP recovered</li>
+                                    <li>Blue to Purple: Effect duration increased</li>
+                                    <li>Orange to orange to orange: +10% EXP, up to a max of +200%</li>
+                                    <li>Yellow to yellow to yellow: +10% class EXP, presumably also maxed at +200%, but I haven't gotten it high enough to be certain</li>
+                                </ul>
+                                <h3>Party bonuses</h3>
+                                
+                                If more than one party member is in overdrive additional bonuses will apply.
+                                
+                                <ul>
+                                    <li>2 members: +50% soul voice activation rate</li>
+                                    <li>3 members: +50 TP per hit with arts</li>
+                                    <li>4 members: gain super armor (all damage halved and immunity to mobility debuffs)</li>
+                                    <li>Overdrive chaining appears to give you half the counter of your ally.</li>
+                                </ul>
+                                <h3>Other notes</h3>
+                                
+                                Sheathing your weapon cancels overdrive. Overdrive becomes available after chapter 5. Skell overdrive is a completely different beast. Many augments and skills affect overdrive. R+left on the D-pad tells your team to enter overdrive.
+                                <br/>
+                                Things I'm still unsure of:
+                                <br/>
+                                I don't think allies in Skell overdrive give you the party bonuses, but I'm not completely sure. If you let overdrive lapse while your party members still have it going and then reactivate it, it will say "overdrive chain" and sometimes the counter does not start at zero, but I'm not sure exactly how this works.
+                                <br/><br/>
+                                <hr/>
+                                <b>Source</b>: <a href ="https://www.reddit.com/r/Xenoblade_Chronicles/comments/3xjf6b/overdrive_explained/" target="_blank">https://www.reddit.com/r/Xenoblade_Chronicles/comments/3xjf6b/overdrive_explained/</a>, by <a href="https://www.reddit.com/user/Anabaena_azollae" target="_blank">Anabaena_azollae</a>
+								
+							</p>
+						</div>
+                                
+                            </p>
+                        </div>
+
+                    </p>
+
+                </p>
+			
+			
+			</article>
+			
+			<div class="paddingBottom">
+			</div>
+			
+			<!-- <footer>
+				2015 birdonwheels5.
+			</footer> -->
+		</div>
+	</body>
+	
+</html>
